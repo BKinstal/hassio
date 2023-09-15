b49613533cf3541e22713b44a1fdaf4f5eb41290
BKinstal <143218188+BKinstal@users.noreply.github.com>
2023-09-15 20:28:47 +0200

    Update sync_git.sh

--- sync_git.sh
+++ sync_git.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 # Nastavte cestu k vašemu repozitáři
-REPO_PATH="/root/config"
+REPO_PATH="/config"
 
 cd $REPO_PATH
 
-# Nastavíme GIT_SSH_COMMAND pro specifikaci klíče (pokud je potřeba)
-#export GIT_SSH_COMMAND="ssh -i /root/config/.ssh/id_rsa -o StrictHostKeyChecking=no"
-
 # Zajistíme, že pracujeme s master větví
-git checkout main
+git checkout master
 
 # Získáme nejnovější změny z vzdáleného repozitáře
 git fetch origin
 
-# Resetujeme lokální main větev na vzdálenou main větev
-git reset --hard origin/main
+# Resetujeme lokální master větev na vzdálenou master větev
+git reset --hard origin/master
